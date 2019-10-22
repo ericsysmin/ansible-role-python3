edccbf501d5dc8dcde43fecd08342205ed2960ca
Eric Anderson <eanderson@avinetworks.com>
2019-10-21 17:20:09 -0700

    add better tests to check for python3

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,19 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_find_command(host):
+    """
+    Test to make sure we can find the python3 executable
+    """
+    exists = host.exists("python3")
+    assert exists
+
+
+def test_run_command(host):
+    """
+    Test that verify's that python3 works
+    """
+    cmd = host.run("python3 -V")
+    assert cmd.succeeded
